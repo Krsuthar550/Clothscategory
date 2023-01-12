753b517af4bd3f11dbc0db83f41cb0ac101cff55
Krsuthar550 <122162697+Krsuthar550@users.noreply.github.com>
2023-01-12 23:29:16 +0530

    Add files via upload

---
+++ Menu.js
@@ -0,0 +1,97 @@
+const Menu = [
+    {
+      id: 1,
+      image: "",
+      name: "Jeans",
+      category: "BottomWear",
+      Gender: "Men",
+      price: "2500₹",
+      description:
+      "Jeans are trousers made from denim. And because the denim’s usually blue, jeans are often called ‘blue jeans."
+    },
+  
+    {
+      id: 2,
+      image: "",
+      name: " Shirt",
+      category: "Topwear",
+      price: "1500₹",
+      description:
+      "A shirt is a cloth garment for the upper body (from the neck to the waist). Originally an undergarment worn exclusively by men, it has become, in American English, a catch-all term for a broad variety of upper-body garments and undergarments"
+    },
+    {
+      id: 3,
+      image: "",
+      name: "Trouser",
+      category: "BottomWear",
+      price: "1800₹",
+      description:
+      "These formal trousers for men by MANCREW can easily spruce up your formal look. Made from Polyester Blend fabric, these slim fit trousers are extremely comfortable to wear and are skin friendly. MANCREW formal trousers are designed keeping you in min"
+    },
+    {
+      id: 4,
+      image: "",
+      name: "Shoes",
+      category: "Footwear",
+      price: "5000₹",
+      description:
+      "A shoe is an item of footwear intended to protect and comfort the human foot. They are often worn with a sock. Shoes are also used as an item of decoration and fashion."
+    },
+    {
+      id: 5,
+      image: "",
+      name: "Tshirt",
+      category: "Topwear",
+      price: "800₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 6,
+      image: "",
+      name: "Formal Shirt",
+      category: "Topwear",
+      price: "1800₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 7,
+      image: "",
+      name: "Shocks",
+      category: "Footwear",
+      price: "Rs 400",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 8,
+      image: "",
+      name: "casual Shoes",
+      category: "Footwear",
+      price: "Rs 6000",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 9,
+      image: "",
+      name: "Polo Tshirt",
+      category: "Topwear",
+      price: "Rs 1000",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 10,
+      image:"",
+      name: "Track Pant",
+      category:"BottomWear",
+      price:"Rs 800",
+      description:"it is used for gym and night wear"
+
+
+    }
+  ];
+  
+  export default Menu;
